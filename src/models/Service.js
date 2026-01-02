53a20306fbc4d8cea2b7d9eba6934c3f856e2e5c
SR Tonmoy <sahriarrahman701@gmail.com>
2026-01-02 19:02:16 +0600

    page details updated with model

--- src/models/Service.js
+++ src/models/Service.js
@@ -1,11 +1,26 @@
 import mongoose from "mongoose";
 
-const serviceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  pricePerDay: { type: Number, required: true },
-  image: { type: String, required: true },
-  category: { type: String }
-}, { timestamps: true });
+const serviceSchema = new mongoose.Schema(
+  {
+    slug: { type: String, unique: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    pricePerDay: { type: Number, required: true },
+    image: { type: String, required: true },
+    category: { type: String },
 
-export default mongoose.models.Service || mongoose.model("Service", serviceSchema);
+    durationOptions: {
+      type: [String],
+      default: []
+    },
+
+    locationOptions: {
+      type: [String],
+      default: []
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Service ||
+  mongoose.model("Service", serviceSchema);
